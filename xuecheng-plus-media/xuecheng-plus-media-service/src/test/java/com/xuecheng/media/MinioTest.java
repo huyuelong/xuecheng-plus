7e96c68c25dc0607affbfe014da42fd611befd90
huyuelong <949910047@qq.com>
2025-06-10 18:29:00 +0800

    test(media): 添加大文件处理和分块上传测试用例
    - 新增 BigFileTest 类，实现大文件分块和合并的测试
    - 在 MinioTest 中添加分块上传和合并的测试方法
    - 验证分块文件上传和合并功能的正确性

--- xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
@@ -3,7 +3,6 @@ package com.xuecheng.media;
 import com.j256.simplemagic.ContentInfo;
 import com.j256.simplemagic.ContentInfoUtil;
 import io.minio.*;
-import io.minio.errors.*;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.compress.utils.IOUtils;
 import org.junit.jupiter.api.Test;
@@ -11,8 +10,9 @@ import org.springframework.http.MediaType;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 测试minio的sdk
@@ -80,5 +80,47 @@ public class MinioTest {
         } else {
             System.out.println("文件上传失败");
         }
+    }
+
+    // 将分块文件上传到minio
+    @Test
+    public void uploadChunk() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            //上传文件的参数信息
+            UploadObjectArgs uploadObjectArgs = UploadObjectArgs.builder()
+                    .bucket("testbucket")//桶
+                    .filename("D:\\94991\\Downs\\chunk\\" + i) //指定本地文件路径
+                    .object("chunk/" + i)//对象名 放在子目录下
+                    .build();
+
+            //上传文件
+            minioClient.uploadObject(uploadObjectArgs);
+            System.out.println("上传分块" + i + "成功");
+        }
+    }
+
+    // 调用minio接口合并分块
+    @Test
+    public void testMerge() throws Exception {
+
+//        List<ComposeSource> sources = null;
+//        for (int i = 0; i < 5; i++) {
+//            // 指定分块文件的信息
+//            ComposeSource composeSource =ComposeSource.builder().bucket("testbucket")
+//                    .object("chunk/" + i)
+//                    .build();
+//            sources.add(composeSource);
+//        }
+
+        List<ComposeSource> sources = Stream.iterate(0, i -> ++i).limit(5).map(i -> ComposeSource.builder().bucket("testbucket").object("chunk/" + i).build()).collect(Collectors.toList());
+
+        // 指定合并后的objectName等信息
+        ComposeObjectArgs composeObjectArgs = ComposeObjectArgs.builder()
+                .bucket("testbucket")
+                .object("merge01.mp4")
+                .sources(sources) // 指定源文件
+                .build();
+        // 合并文件
+        minioClient.composeObject(composeObjectArgs);
     }
 }
