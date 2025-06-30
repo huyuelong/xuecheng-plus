f53e3f2ba8acd7311d43c235af621c7360d4ee73
huyuelong <949910047@qq.com>
2025-06-30 14:50:22 +0800

    feat(auth): 实现微信扫码登录功能
    - 添加微信扫码登录相关的接口和服务实现
    - 集成 Feign 客户端用于验证验证码
    - 新增 RestTemplate 用于调用微信接口
    - 实现微信用户信息获取和数据库保存逻辑
    - 添加微信登录控制器处理登录请求

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/WxAuthServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/WxAuthServiceImpl.java
@@ -1,15 +1,175 @@
 package com.xuecheng.ucenter.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.ucenter.mapper.XcUserMapper;
+import com.xuecheng.ucenter.mapper.XcUserRoleMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.dto.XcUserExt;
+import com.xuecheng.ucenter.model.po.XcUser;
+import com.xuecheng.ucenter.model.po.XcUserRole;
 import com.xuecheng.ucenter.service.AuthService;
+import com.xuecheng.ucenter.service.WxAuthService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * 微信扫码认证
+ */
 @Service("wx_authservice")
-public class WxAuthServiceImpl implements AuthService {
+public class WxAuthServiceImpl implements AuthService, WxAuthService {
+
+    @Autowired
+    XcUserMapper xcUserMapper;
+
+    @Autowired
+    XcUserRoleMapper xcUserRoleMapper;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WxAuthServiceImpl currentPorxy;
+
+    @Value("${weixin.appid}")
+    private String appid;
+
+    @Value("${weixin.secret}")
+    private String secret;
 
     @Override
     public XcUserExt execute(AuthParamsDto authParamsDto) {
-        return null;
+        // 得到子账号
+        String username = authParamsDto.getUsername();
+        // 查询数据量
+        XcUser xcUser = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
+        if (xcUser == null) {
+            throw new RuntimeException("用户不存在");
+        }
+
+        XcUserExt xcUserExt = new XcUserExt();
+        BeanUtils.copyProperties(xcUser, xcUserExt);
+        return xcUserExt;
     }
+
+    @Override
+    public XcUser wxAuth(String code) {
+        // 申请令牌
+        Map<String, String> access_token = getAccess_token(code);
+        // 访问令牌
+        String accessToken = access_token.get("access_token");
+        String openid = access_token.get("openid");
+
+        // 携带令牌查询用户信息
+        Map<String, String> userInfo = getUserInfo(accessToken, openid);
+
+        // 保存用户信息到数据库
+        XcUser xcUser = currentPorxy.addWxUser(userInfo);
+
+        return xcUser;
+    }
+
+    @Transactional
+    public XcUser addWxUser(Map<String, String> userInfo_map) {
+        String unionid =userInfo_map.get("unionid");
+        String nickname = userInfo_map.get("nickname");
+        // 根据unionid查询用户信息
+        XcUser xcUser = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getWxUnionid, unionid));
+        if (xcUser != null) {
+            return xcUser;
+        }
+        // 向数据库新增记录
+        xcUser = new XcUser();
+        String userId = UUID.randomUUID().toString();
+        xcUser.setId(userId); //主键
+        xcUser.setUsername(unionid);
+        xcUser.setPassword(unionid);
+        xcUser.setWxUnionid(unionid);
+        xcUser.setName(nickname);
+        xcUser.setName(nickname);
+        xcUser.setUtype("101001"); // 学生类型
+        xcUser.setStatus("1"); // 用户状态
+        xcUser.setCreateTime(LocalDateTime.now());
+        // 插入
+        xcUserMapper.insert(xcUser);
+
+        // 向用户角色关系表新增记录
+        XcUserRole xcUserRole = new XcUserRole();
+        xcUserRole.setId(UUID.randomUUID().toString());
+        xcUserRole.setUserId(userId);
+        xcUserRole.setRoleId("17"); // 学生角色
+        xcUserRole.setCreateTime(LocalDateTime.now());
+        xcUserRoleMapper.insert(xcUserRole);
+        return xcUser;
+    }
+
+    /**
+     * 申请访问令牌,响应示例
+     {
+     "access_token":"ACCESS_TOKEN",
+     "expires_in":7200,
+     "refresh_token":"REFRESH_TOKEN",
+     "openid":"OPENID",
+     "scope":"SCOPE",
+     "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+     }
+     */
+    private Map<String,String> getAccess_token(String code) {
+
+        String url_template = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
+        String url = String.format(url_template, appid, secret, code);
+
+        // 远程调用此url
+        ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, null, String.class);
+        // 获取响应的结果
+        String body = exchange.getBody();
+        // 将result 转为map
+        Map<String,String> map = JSON.parseObject(body, Map.class);
+        return map;
+    }
+
+    /**
+     * 携带令牌查询用户信息
+     * @param access_token
+     * @param openid
+     * @return
+     */
+    /**获取用户信息，示例如下：
+     {
+     "openid":"OPENID",
+     "nickname":"NICKNAME",
+     "sex":1,
+     "province":"PROVINCE",
+     "city":"CITY",
+     "country":"COUNTRY",
+     "headimgurl": "https://thirdwx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/0",
+     "privilege":[
+     "PRIVILEGE1",
+     "PRIVILEGE2"
+     ],
+     "unionid": " o6_bmasdasdsad6_2sgVt7hMZOPfL"
+     }
+     */
+    private Map<String,String> getUserInfo(String access_token, String openid) {
+
+        String url_template = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s";
+        String url = String.format(url_template, access_token, openid);
+
+        ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        String body = new String(exchange.getBody().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+        Map<String,String> map = JSON.parseObject(body, Map.class);
+        return map;
+    }
+
 }
